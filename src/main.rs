b92522fcc286844b265f9ebe88026f7e8a8a821d
Abigail G <dev@kb6.ee>
2021-05-07 00:02:18 -0400

    some small changes/cleanups

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 
 use std::{fs::File, io::BufRead};
 use std::io::{BufReader, stdin};
+use std::io::Error as IOError;
 use std::path::Path;
 
 use clap::{Arg, App};
@@ -29,26 +30,19 @@ fn main() {
 fn get_path(s: Option<&str>) -> Option<&Path> {
     match s {
         Some(p) if p != "-" => Some(&Path::new(p)),
-        Some(_) => None,
-        None => None
+        Some(_) | None => None,
     }
 }
 
 fn process_file(path: &Path) {
     let buffer = BufReader::new(match File::open(path) {
         Ok(f) => f,
-        Err(e) => {
-            eprintln!("{}", e);
-            std::process::exit(1);
-        }
+        Err(e) => print_io_error_and_exit(e, 1),
     });
     for line in buffer.lines() {
         match line {
             Ok(s) => println!("{}", s),
-            Err(e) => {
-                eprintln!("{}", e);
-                std::process::exit(1);
-            }
+            Err(e) => print_io_error_and_exit(e, 1),
         };
     }
 }
@@ -58,10 +52,13 @@ fn process_stdin() {
     for line in input.lock().lines() {
         match line {
             Ok(s) => println!("{}", s),
-            Err(e) => {
-                eprintln!("{}", e);
-                std::process::exit(1);
-            }
+            Err(e) => print_io_error_and_exit(e, 1),
         };
     }
+}
+
+//? This maybe should be generalised/macroised in the future
+fn print_io_error_and_exit(error: IOError, code: i32) -> ! {
+    eprintln!("{}", error);
+    std::process::exit(code);
 }
